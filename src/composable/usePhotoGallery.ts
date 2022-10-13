fbde94444163d69a10a1b28bfb72141a6ea515e4
Juan Carlos <jc203039@gmail.com>
2022-10-13 16:19:24 -0300

    primeiro commit

---
+++ src/composable/usePhotoGallery.ts
@@ -0,0 +1,107 @@
+import { ref, onMounted, watch } from 'vue';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { stringifyQuery } from 'vue-router';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
+
+
+export interface userPhoto {
+    filepath: string;
+    webviewPath?: string;
+}
+
+export function usePhotoGallery() {
+    const photos = ref<userPhoto[]>([]);
+    const PHOTO_STORAGE = 'photos';
+
+    const cachePhotos = () => {
+        Preferences.set({
+            key: PHOTO_STORAGE,
+            value: JSON.stringify(photos.value),
+        });
+    };
+
+    const convertBlobToBase64 = (blob: Blob) =>
+        new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onerror = reject;
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+            reader.readAsDataURL(blob);
+        });
+
+    const savePicture = async (photo: Photo, fileName: string):
+        Promise<userPhoto> => {
+        let base64Data: string;
+        if (isPlatform('hybrid')) {
+            const file = await Filesystem.readFile({
+                path: photo.path!,
+            });
+            base64Data = file.data;
+        } else {
+            const response = await fetch(photo.webPath!);
+            const blob = await response.blob();
+            base64Data = (await convertBlobToBase64(blob)) as string;
+        }
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+        if (isPlatform('hybrid')) {
+            return {
+                filepath: savedFile.uri,
+                webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+            };
+        } else {
+            return {
+                filepath: fileName,
+                webviewPath: photo.webPath,
+            };
+        }
+    };
+
+    const takePhoto = async () => {
+        const photo = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100,
+        });
+
+        const fileName = new Date().getTime() + '.jpeg';
+        const savedFileImage = await savePicture(photo, fileName);
+
+        photos.value = [savedFileImage, ...photos.value];
+
+
+    };
+
+    const loadSave = async () => {
+        const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+        const photoInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
+
+        if (!isPlatform('hybrid')) {
+            for (const photo of photoInPreferences) {
+                const file = await Filesystem.readFile({
+                    path: photo.filepath,
+                    directory: Directory.Data,
+                });
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+            }
+            photos.value = photoInPreferences;
+        }
+
+    };
+
+
+    onMounted(loadSave);
+    watch(photos, cachePhotos);
+
+    return {
+        photos,
+        takePhoto,
+    };
+}
---
+++ src/views/TabsPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="triangle" />
+          <ion-label>Tab 1</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="ellipse" />
+          <ion-label>Tab 2</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="square" />
+          <ion-label>Tab 3</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="apertureOutline" />
+          <ion-label>Foto</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+          <ion-icon :icon="appsOutline" />
+          <ion-label>Número</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { ellipse, square, triangle,apertureOutline,appsOutline } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'Tabs5Page',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  setup() {
+    return {
+      ellipse, 
+      square, 
+      triangle,
+      apertureOutline,
+      appsOutline
+    }
+  }
+});
+</script>
